d330eb035a83e9e54ccc79fca6e3deb9dd7d0d30
Sophia Mazurek <sophiamazurek@Sophias-MBP-2.lan>
2021-03-14 12:14:58 -0500

    cleaned up code

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -9,12 +9,12 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     
-      id:{
+    id:{
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true,
-      },
+    },
 
     product_id:{
     type: DataTypes.INTEGER,
--- models/index.js
+++ models/index.js
@@ -18,7 +18,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { through: ProductTag, 
   foreignKey: 'tag_id',
-  });
+});
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { through: ProductTag, 
