19b8de0834feda2e9a23246f3e8bafaefdba0041
Sophia Mazurek <sophiamazurek@Sophias-MBP-2.lan>
2021-03-15 21:57:49 -0500

    product route completed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,27 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [Category, Tag]
+  })
+  .then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Category, Tag]
+
+  }).then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
 // create new product
@@ -91,6 +106,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
 module.exports = router;
