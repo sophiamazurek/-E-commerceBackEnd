1d0da809f5fe838b310b8b78ddd87206f300fe08
Sophia Mazurek <sophiamazurek@Sophias-MBP-2.lan>
2021-03-15 21:47:21 -0500

    category routes completed

--- models/index.js
+++ models/index.js
@@ -12,12 +12,12 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_name',
+  foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, { through: ProductTag, 
-  foreignKey: 'tag_id',
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,10 +6,11 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // ****be sure to include its associated Products
-
+//res.send("works");
   Category.findAll({
-    
-  }).then(dbPost => {
+    include: [Product]
+  })
+  .then(dbPost => {
     res.json(dbPost);
   });
 });
@@ -17,18 +18,52 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+
+  }).then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  console.log(req.body);
+  Category.create(
+   req.body
+  ).then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+     //update userinput
+      req.body
+    ,
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
 module.exports = router;
