fd7f51727435615f2223a94dc4cd71e9484a61b4
Sophia Mazurek <sophiamazurek@Sophias-MacBook-Pro-2.local>
2021-03-16 09:45:11 -0500

    completed tag route, all routes are completed without errors

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,63 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product]
+  })
+  .then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+
+  }).then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  console.log(req.body);
+  Tag.create(
+   req.body
+  ).then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    //update userinput
+     req.body
+   ,
+   {
+     where: {
+       id: req.params.id
+     }
+   }
+ ).then(dbPost => {
+   res.json(dbPost);
+ });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbPost => {
+    res.json(dbPost);
+  });
 });
 
 module.exports = router;
